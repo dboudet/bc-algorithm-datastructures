f484566a6bb2535eda293cfc5f18df6c4a8b9607
Dan Boudet <dboudet@palmbeachculture.com>
2021-07-07 14:58:30 -0400

    added details to console log for results

--- jsPractice070721.js
+++ jsPractice070721.js
@@ -1,42 +1,28 @@
 testArray = [123,456,789,987,654,321,193,828,922]
+console.log("This is the test array:" , testArray)
 
 // function to get the first element in an array
-
-/* 
 function getFirstElementInArray (array) {
-    console.log(array[0])
+    return array[0]
 }
-getFirstElementInArray(testArray)
-*/
+console.log("First number in array:" , getFirstElementInArray(testArray))
 
 //function to get last element in array
-
-/* 
 function getLastElementInArray (array) {
     const lastNum = array.length - 1
-    console.log(array[lastNum])
+    return array[lastNum]
 }
+console.log("Last number in array:" , getLastElementInArray(testArray))
 
-getLastElementInArray(testArray)
- */
-
-
-//function to get given element of an array passed as parameter
-
-/* 
+//function to get value of number at index [] of array passed as parameter
 const testIndex = 2
-
 function getIndex (index) {
     return testArray[index]
 }
-
-console.log (getIndex(testIndex))
- */
+console.log ("This value is at index", testIndex , "of the array:" , getIndex(testIndex))
 
 //function to get the index of an element in an array where its element matches a value passed as a parameter
-
 const testValue = 321
-
 function findIndexByValue (value,array) {
     for (let i=0; i<array.length; i++) {
         if (array[i] === value){
@@ -44,5 +30,4 @@ function findIndexByValue (value,array) {
         }
     }
 }
-
-console.log(findIndexByValue(testValue,testArray))
+console.log("The number ", testValue , "is at index:" , findIndexByValue(testValue,testArray))
